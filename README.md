7fcf44689f861787cdab23fcadb26316f9de048c
cyrus <j07424-development@yahoo.co.jp>
2020-03-16 13:11:58 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# M5stickV playground
+
+---
+
+## Target firmware
+
+- M5StickV_Firmware_1022_beta.kfpkg
+- maixpy_v0.5.0_9_g8eba07d_m5stickv.bin
---
+++ template.py
@@ -0,0 +1,29 @@
+#
+# Template.
+#
+
+##################################################
+# import
+##################################################
+import sensor, image, time, lcd
+
+##################################################
+# initialize
+##################################################
+lcd.init(freq=15000000)
+sensor.reset()                      # Reset and initialize the sensor. It will
+                                    # run automatically, call sensor.run(0) to stop
+sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
+sensor.skip_frames(time = 2000)     # Wait for settings take effect.
+clock = time.clock()                # Create a clock object to track the FPS.
+
+##################################################
+# main
+##################################################
+while(True):
+    clock.tick()                    # Update the FPS clock.
+    img = sensor.snapshot()         # Take a picture and return the image.
+    lcd.display(img)                # Display on LCD
+    print(clock.fps())              # Note: MaixPy's Cam runs about half as fast when connected
+                                    # to the IDE. The FPS should increase once disconnected.
